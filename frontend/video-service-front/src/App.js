912cbc1cd0c804b6055d8f670890db1091591c84
Noname-creat0r <egorchik228322@gmail.com>
2022-11-29 16:46:45 +0300

    0.1.15: Routes were added
    2 simple routes now. (Home and Auth)

--- frontend/video-service-front/src/App.js
+++ frontend/video-service-front/src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import { Route, Routes, Navigate} from 'react-router-dom';
 
 import Layout from './containers/Layout/Layout';
 import ErrorBoundary from './hoc/ErrorBoundary/ErrorBoundary';
+import Auth from './containers/Auth/Auth';
 
 import './App.css';
 
+const Home = () => <h1> It's home, friend </h1>
+//const Auth = React.lazy(() => import('./containers/Auth/Auth'));
+
 const App = () => {
+  let routes = (
+    <Routes>
+      <Route path="/auth" element={<Auth />} />
+      <Route path="/" element={Home} />
+    </Routes>
+  );
+
+
   return (
     <div className="App">
       <ErrorBoundary>
         <Layout>
-          dad
+          {routes}
         </Layout>
       </ErrorBoundary>
     </div>
