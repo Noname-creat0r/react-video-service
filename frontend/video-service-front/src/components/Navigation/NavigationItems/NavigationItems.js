0c7bc76356dcc1e19c01fa9ae8744876bcf1f35a
Noname-creat0r <egorchik228322@gmail.com>
2022-11-29 16:52:23 +0300

    0.1.16: Making links in SideDrawer alive
    NavigationItems link to 2 simple routs from 912cbc1cd0c804b6055d8f670890db1091591c84

--- frontend/video-service-front/src/components/Navigation/NavigationItems/NavigationItems.js
+++ frontend/video-service-front/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,9 +4,9 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = (props) => (
     <ul className='NavigationItems'>
-        <NavigationItem link="/" exact>Home</NavigationItem>
-        <NavigationItem link="/" exact>Profile</NavigationItem>
-        <NavigationItem link="/" exact>Settings</NavigationItem>
+        <NavigationItem link="/">Home</NavigationItem>
+        <NavigationItem link="/auth">Profile</NavigationItem>
+        <NavigationItem link="/" >Settings</NavigationItem>
     </ul>
 );
 
--- frontend/video-service-front/src/components/Navigation/Toolbar/Toolbar.js
+++ frontend/video-service-front/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Navigate } from 'react-router-dom';
 
 import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 import SearchInput from "../SearchInput/SearchInput";
@@ -7,13 +8,20 @@ import UserName from "../../UI/User/UserName/UserName";
 
 import './Toolbar.css';
 
+const userClickHandler = () => {
+    <Navigate to="/auth" replace={true}/>;
+};
+
 const toolbar = (props) => (
+    // TODO: Wrapper for User section
+
     <header>
         <nav className="navbar">
             <DrawerToggle clicked={props.drawerToggleClicked}/>
             <SearchInput />
             
-            <div className="d-flex me-2">
+            <div className="d-flex me-2"
+                onClick={() => userClickHandler}>
                 <UserIcon />
                 <UserName />
             </div>
