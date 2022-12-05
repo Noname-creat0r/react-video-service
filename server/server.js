2930bd9bf9167172697d4e7290d77dde01b8d7cb
Noname-creat0r <egorchik228322@gmail.com>
2022-12-05 18:28:10 +0300

    0.1.25: Adding basic server logic
    It's gonna be a REST server.

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { default: mongoose } = require('mongoose');
+
+const app = express();
+const port = 8080;
+const baseUrl = "mongodb://127.0.0.1:27017/ReactVideoService";
+
+app.use (bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods',
+        'OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+// Error handling middleware
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+    .connect(baseUrl)
+    .then(result => {
+        app.listen(port);
+
+    })
+    .catch(err => console.log(err));
