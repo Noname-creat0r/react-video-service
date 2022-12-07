1308c5953b736c414e2646c7f8028986bd139b96
Noname-creat0r <egorchik228322@gmail.com>
2022-12-07 23:09:02 +0300

    0.1.30: Adding utility and axios base settings

--- frontend/video-service-front/src/App.js
+++ frontend/video-service-front/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Route, Routes} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Routes, withRouter} from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Layout from './containers/Layout/Layout';
 import ErrorBoundary from './hoc/ErrorBoundary/ErrorBoundary';
@@ -9,27 +10,41 @@ import Profile from './containers/User/Profile/Profile';
 import './App.css';
 
 const Home = () => <h1> It's home, friend </h1>
-//const Auth = React.lazy(() => import('./containers/Auth/Auth'));
 
-const App = () => {
-  let routes = (
-    <Routes>
-      <Route path="/auth" element={<Auth />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/" element={Home} />
-    </Routes>
-  );
+class App extends Component {
 
+  render () {
+
+    let routes = (
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={Home} />
+      </Routes>
+    );
 
   return (
-    <div className="App">
-      <ErrorBoundary>
-        <Layout>
-          {routes}
-        </Layout>
-      </ErrorBoundary>
-    </div>
-  );
+      <div className="App">
+        <ErrorBoundary>
+          <Layout>
+            {routes}
+          </Layout>
+        </ErrorBoundary>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    //isAuthenticated: state.auth.token !== null
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    //onTryAutoSignup: () => dispatch( actions.authCheckState() )
+  };
+};
+
+export default connect( mapStateToProps, mapDispatchToProps ) ( App ) ;
