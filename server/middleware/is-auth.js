e299b21c6d61a0c2d2f36108ee0a2781635c41fa
Noname-creat0r <egorchik228322@gmail.com>
2022-12-08 18:46:47 +0300

    0.1.34: Adding is-auth middleware to the server

---
+++ server/middleware/is-auth.js
@@ -0,0 +1,27 @@
+const jsw = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    const authHeader = req.get('Authorization');
+    if (!authHeader) {
+        const error = new Error('Not authenticated.');
+        error.statusCode = 401;
+        throw error;
+    }
+
+    const token = authHeader.split(' ')[1];
+    let decodedToken;
+    try {
+        decodedToken = jwt.verify(token, '172b-3c4g-5w6O-7x8h');
+    } catch(err) {
+        err.statusCode = 500;
+        throw err;
+    }
+    if (!decodedToken) {
+        const error = new Error('Not authenticated.');
+        error.statusCode = 401;
+        throw error;
+    }
+
+    req.userId = decodedToken.userId;
+    next();
+};
